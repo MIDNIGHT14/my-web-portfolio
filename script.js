e8a5e673129a7427992a024a8a527f678e98029e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-07-18 09:55:09 +0800

    Added skills section

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const menuButton = document.getElementById('menu-button')
 const mobileNav = document.getElementById('nav-links-mobile')
 
 menuButton.addEventListener('click', () => {
-  mobileNav.classList.toggle('block')
   mobileNav.classList.toggle('hidden')
+  mobileNav.classList.toggle('block')
 })
